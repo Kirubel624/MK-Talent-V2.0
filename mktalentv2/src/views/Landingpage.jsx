2e9fa712d3e41119e5bd19ee79b5e4e4cf44c5bb
kirubel <kirato747@gmail.com>
2023-11-15 14:33:21 +0300

    Configure tailwindcss to work with vite

---
+++ mktalentv2/src/views/Landingpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landingpage = () => {
+  return (
+    <div className=''>
+      <p>Landing page</p>
+    </div>
+  )
+}
+
+export default Landingpage
--- mktalentv2/tailwind.config.js
+++ mktalentv2/tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+       keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
+      animation: {
+        'float': 'float 3s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 }
